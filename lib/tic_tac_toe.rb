10162573d3ccec6826d42f4a7958f755fb55857a
Alex Engelhart <asengelhart@yahoo.com>
2019-12-19 19:59:49 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -97,10 +97,12 @@ class TicTacToe
     while !over? do 
       turn 
     end 
+    
     if won?
       puts "Congratulations #{winner}!"
     elsif draw?
       puts "Cat's Game!"
     end 
-  end 
+  end
+  
 end 
